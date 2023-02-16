ae511f1c2e48e59bf2c1538034326fc1272428a8
Shineylo <bryan.artiges@gmail.com>
2023-02-16 16:47:11 +0100

    Init Commit

---
+++ src/main/java/technobel/bart/laboschool/LaboSchoolApplication.java
@@ -0,0 +1,13 @@
+package technobel.bart.laboschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/technobel/bart/laboschool/LaboSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package technobel.bart.laboschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
